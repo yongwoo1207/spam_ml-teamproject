1fe093a0896583e15ab11349064b44526c466199
hansokchun <127171688+hansokchun@users.noreply.github.com>
2023-12-15 18:29:25 +0900

    Update email_spam_classification.py

--- SVM/email_spam_classification.py
+++ SVM/email_spam_classification.py
@@ -25,19 +25,24 @@ y_pred_SVM_nonlinear = model_SVM_nonlinear.predict(X_test)
 accuracy_SVM = accuracy_score(y_test, y_pred_SVM)
 precision_SVM = precision_score(y_test, y_pred_SVM)
 recall_SVM = recall_score(y_test, y_pred_SVM)
+f1_SVM = f1_score(y_test, y_pred_SVM)
 
 accuracy_SVM_nonlinear = accuracy_score(y_test, y_pred_SVM_nonlinear)
 precision_SVM_nonlinear = precision_score(y_test, y_pred_SVM_nonlinear)
 recall_SVM_nonlinear = recall_score(y_test, y_pred_SVM_nonlinear)
+f1_SVM_nonlinear = f1_score(y_test, y_pred_SVM_nonlinear)
 
 
 # 평가 출력
 results["SVM"] = {"accuracy": accuracy_SVM, 
                   "precision": precision_SVM, 
                   "recall": recall_SVM, 
+                   "f1_score": f1_SVM
                  }
 results["SVM_nonlinear"] = {"accuracy": accuracy_SVM_nonlinear, 
                   "precision": precision_SVM_nonlinear, 
                   "recall": recall_SVM_nonlinear, 
+                             "f1_score": f1_SVM_nonlinear
+                            
                  }
 results
